440e23af40462f6cb7ea791c520dbb7ac94633d3
Wilker Chaves <chaveswill8@gmail.com>
2024-11-28 23:22:04 -0300

    Criando o service de usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './models/user';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Projeto-Cadastro-Usuario';
+export class AppComponent implements OnInit {
+
+  usersList: Array<User> = []
+
+  constructor(private readonly _userService: UsersService) {
+
+  }
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this._userService.getUsers().subscribe((usersListResponse) => {
+      this.usersList = usersListResponse
+    })
+  }
 }
+
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { User } from '../models/user';
 
 @Injectable({
@@ -7,5 +8,90 @@ import { User } from '../models/user';
 export class UsersService {
 
   private readonly userList: Array<User> = [
+    {
+      name: "Usuario 1",
+      username: "usuario.one",
+      email: "usu1@email.com",
+      birthDate: "01/01/2001",
+      state: 13,
+      password: "AW21@!ssa1@",
+      musics: [
+        {
+          title: "Music 1", band: "Band 1", genre: 1, isFavorite: false
+        },
+        {
+          title: "Music 2", band: "Band 1", genre: 2, isFavorite: true
+        },
+        {
+          title: "Music 3", band: "Band 1", genre: 3, isFavorite: false
+        },
+      ]
+    },
+    {
+      name: "Usuario 1",
+      username: "usuario.one",
+      email: "usu1@email.com",
+      birthDate: "01/01/2001",
+      state: 13,
+      password: "AW21@!ssa1@",
+      musics: [
+        {
+          title: "Music 1", band: "Band 1", genre: 1, isFavorite: false
+        },
+        {
+          title: "Music 2", band: "Band 1", genre: 2, isFavorite: true
+        },
+        {
+          title: "Music 3", band: "Band 1", genre: 3, isFavorite: false
+        },
+      ]
+    },
+    {
+      name: "Usuario 1",
+      username: "usuario.one",
+      email: "usu1@email.com",
+      birthDate: "01/01/2001",
+      state: 13,
+      password: "AW21@!ssa1@",
+      musics: [
+        {
+          title: "Music 1", band: "Band 1", genre: 1, isFavorite: false
+        },
+        {
+          title: "Music 2", band: "Band 1", genre: 2, isFavorite: true
+        },
+        {
+          title: "Music 3", band: "Band 1", genre: 3, isFavorite: false
+        },
+      ]
+    },
+    {
+      name: "Usuario 1",
+      username: "usuario.one",
+      email: "usu1@email.com",
+      birthDate: "01/01/2001",
+      state: 13,
+      password: "AW21@!ssa1@",
+      musics: [
+        {
+          title: "Music 1", band: "Band 1", genre: 1, isFavorite: false
+        },
+        {
+          title: "Music 2", band: "Band 1", genre: 2, isFavorite: true
+        },
+        {
+          title: "Music 3", band: "Band 1", genre: 3, isFavorite: false
+        },
+      ]
+    }
   ]
+
+  getUsers(): Observable<Array<User>> {
+    return new Observable((observer) => {
+      setTimeout(() => {
+        observer.next(this.userList)
+        observer.complete()
+      }, 3000)
+    })
+  }
 }
